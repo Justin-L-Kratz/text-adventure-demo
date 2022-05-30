0fd12f7b5aa0a5f73aae0fe23fb824b1fa7bb700
Justin Kratz <justin.l.kratz@outlook.com>
2022-05-30 01:01:54 -0400

    Created rooms for the dungeon

---
+++ rooms.py
@@ -0,0 +1,91 @@
+import enemies
+
+room1 = {
+    "containsChest": False,
+    "containsEnemy": False
+}
+
+room2 = {
+    "containsChest": False,
+    "containsEnemy": False
+}
+
+room3 = {
+    "containsChest": False,
+    "containsEnemy": enemies.kobold0
+}
+
+room4 = {
+    "containsChest": False,
+    "containsEnemy": False
+}
+
+room5 = {
+    "containsChest": True,
+    "containsEnemy": False
+}
+
+room6 = {
+    "containsChest": False,
+    "containsEnemy": enemies.kobold1
+}
+
+room7 = {
+    "containsChest": False,
+    "containsEnemy": False
+}
+
+room8 = {
+    "containsChest": False,
+    "containsEnemy": False
+}
+
+room9 = {
+    "containsChest": True,
+    "containsEnemy": False
+}
+
+room10 = {
+    "containsChest": False,
+    "containsEnemy": enemies.kobold2
+}
+
+room11 = {
+    "containsChest": False,
+    "containsEnemy": False
+}
+
+room12 = {
+    "containsChest": False,
+    "containsEnemy": False
+}
+
+room13 = {
+    "containsChest": True,
+    "containsEnemy": False
+}
+
+room14 = {
+    "containsChest": False,
+    "containsEnemy": False
+}
+
+room15 = {
+    "containsChest": False,
+    "containsEnemy": enemies.kobold3
+}
+
+room16 = {
+    "containsChest": False,
+    "containsEnemy": False
+}
+
+room17 = {
+    "containsChest": True,
+    "containsEnemy": False
+}
+
+room18 = {
+    "containsChest": False,
+    "containsEnemy": enemies.koboldB
+}
