783a611d4dccd76b78314cffc2968a613cff3701
Justin Kratz <justin.l.kratz@outlook.com>
2022-05-30 01:02:54 -0400

    Created enemies for the rooms

---
+++ enemies.py
@@ -0,0 +1,34 @@
+kobold0 = {
+    "name": "Kobold Minion",
+    "hp": 10,
+    "attack": 5,
+    "alive": True 
+}
+
+kobold1 = {
+    "name": "Kobold Minion",
+    "hp": 10,
+    "attack": 5,
+    "alive": True
+}
+
+kobold2 = {
+    "name": "Kobold Minion",
+    "hp": 10,
+    "attack": 5,
+    "alive": True
+}
+
+kobold3 = {
+    "name": "Kobold Minion",
+    "hp": 10,
+    "attack": 5,
+    "alive": True
+}
+
+koboldB = {
+    "name": "Kobold Minion",
+    "hp": 25,
+    "attack": 5,
+    "alive": True
+}
