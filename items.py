f7a08e9ad79934481176e6fe2b3ddaf9896d34b5
Justin Kratz <justin-l-kratz@outlook.com>
2022-06-02 03:03:01 -0400

    File created, chests reformatted

---
+++ items.py
@@ -0,0 +1,32 @@
+potion1 = {
+    "description": "A potion that restores your vitality, leaving you feeling refreshed"
+}
+
+potion2 = {
+    "description": "A potion that restores your vitality, leaving you feeling refreshed"
+}
+
+potion3 = {
+    "description": "A potion that restores your vitality, leaving you feeling refreshed"
+}
+
+potion4 = {
+    "description": "A potion that restores your vitality, leaving you feeling refreshed"
+}
+
+chest1 = {
+    "open": False,
+    "contents": potion1
+}
+chest2 = {
+    "open": False,
+    "contents": potion2
+}
+chest3 = {
+    "open": False,
+    "contents": potion3
+}
+chest4 = {
+    "open": False,
+    "contents": potion4
+}
