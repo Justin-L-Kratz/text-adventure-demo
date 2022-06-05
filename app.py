beaef4349749a0d7e4f358d6934609d51e8f9376
Justin Kratz <justin.l.kratz@outlook.com>
2022-06-04 21:44:38 -0400

    Completed move and attack functions

--- app.py
+++ app.py
@@ -3,27 +3,6 @@ game_state = True
 def init():
     import rooms
 
-    dungeon = [
-        rooms.room1,
-        rooms.room2,
-        rooms.room3,
-        rooms.room4,
-        rooms.room5,
-        rooms.room6,
-        rooms.room7,
-        rooms.room8,
-        rooms.room9,
-        rooms.room10,
-        rooms.room11,
-        rooms.room12,
-        rooms.room13,
-        rooms.room14,
-        rooms.room15,
-        rooms.room16,
-        rooms.room17,
-        rooms.room18
-    ]
-
     player = {
         "hp": 30,
         "attack": 5,
@@ -42,8 +21,17 @@ def init():
             print("You can see no such item in this room")
 
     def move(direction):
-        pass
-    
-    print(player["location"] + dungeon[player["location"]]["north"])    
+        if rooms.dungeon[player["location"]][direction] != False:
+            player["location"] += rooms.dungeon[player["location"]][direction]
+            print(player["location"])
+        else:
+            print("You can not go that way")
+
+    def attack(target):
+        target["hp"] -= 5
+        battle_text = "You attack {} for 5 damage"
+        print(battle_text.format(target["name"]))
+
+    attack(rooms.enemies.kobold0)
 
 init()   
